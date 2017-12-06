6e37ca28f06966ce5d8612cc363a6211ef724577
Gu Muthulaskhmi <mislakshmi006@gmail.com>
2017-12-06 21:49:09 +0530

    learn python the hard way ex1

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#-*-coding: utf-8 -*-
+#this is 1st 
+print "hello world"
+print "i like this"
+print "this is fun."
+print 'ya printing'
+print"i'd much rather you'not'."
+print'"siad"do not touch this.'
+
