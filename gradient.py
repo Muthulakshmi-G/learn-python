9c321e51a666f3125eb4aac2bbd79a54dfc7d5b7
jayamuthu <33984173+jayamuthu@users.noreply.github.com>
2018-02-15 11:48:19 +0530

    gradient descent

---
+++ gradient.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+data_x=np.linspace(1.0,10.0,100)[:,np.newaxis]
+data_y=np.sin(data_x)+0.1*np.power(data_x,2)+0.5*np.random.randn(100,1)
+data_x/=np.max(data_x)
+
+
+
+
+data_x=np.hstack((np.ones_like(data_x),data_x))
+
+order=np.random.permutation(len(data_x))
+
+portion=20
+
+test_x=data_x[order[:portion]]
+test_y=data_y[order[:portion]]
+
+train_x=data_x[order[portion:]]
+train_y=data_y[order[portion:]]
+
+#create gradient function
+def get_gradient(w,x,y):
+    y_estimate=x.dot(w).flatten()
+    error=(y.flatten()-y_estimate)
+    mse=(1.0/len(x))*np.sum(np.power(error,2))
+    gradient=-(1.0/len(x))*error.dot(x)
+    return gradient,mse
+
+
+#perform GD to learn model
+w=np.random.randn(2)
+alpha=0.5
+tolerance=1e-5
+
+#perform gradient descent
+iterations=1
+while True:
+        gradient,error=get_gradient(w,train_x,train_y)
+        new_w=w-alpha*gradient
+        
+#stopping condition
+        if np.sum(abs(new_w))<tolerance:
+            print("converged")
+            break
+
+#print error every 50 iterations
+        if iterations%100==0:
+            print("iteration: %d-error:% .4f"%(iterations,error))
+
+        iterations+=1
+        w=new_w
+
+print("w=",w)
+print("test cost=",get_gradient(w,test_x,test_y))[1]
+
+
+
+
+plt.plot(data_x[:,1], data_x.dot(w), c='g', label='Model')
+plt.scatter(train_x[:,1], train_y, c='b', label='Train Set')
+plt.scatter(test_x[:,1], test_y, c='r', label='Test Set')
+plt.grid()
+plt.legend(loc='best')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
